c973281b1876eb4dc2a86c5e8a88c0478a7e9151
Ashley Camba <ashwoods@gmail.com>
2014-09-26 18:09:38 +0200

    pep8 + hstore refactor

--- formulator/models.py
+++ formulator/models.py
@@ -1,3 +1,4 @@
+
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 import importlib
@@ -12,15 +13,18 @@ from positions import PositionField
 from crispy_forms.helper import FormHelper
 from crispy_forms import layout
 
+
 import floppyforms as forms
-import jsonfield
+from django_hstore import hstore
 
 from formulator.conf import settings
 
-
+@python_2_unicode_compatible
 class Form(models.Model):
     """
-    Form class
+    Model that defines a django Form class. The form_class_factory method returns a Form class with a default base of
+    a floppy form Form class. It uses crispyforms helpers to add the additional form properties that django forms don't
+    handle out of the box. It also uses crispyform fieldsets per default.
     """
 
     ENCTYPES = Choices((0, 'urlencoded', 'application/x-www-form-urlencoded'),
@@ -33,13 +37,14 @@ class Form(models.Model):
 
     # HTML5 FORM ATTRIBUTES for crispy forms
 
-    # crispy-forms helper attributes
+    # common form attributes
     form_name = models.CharField(max_length=100, blank=True)
     form_action = models.CharField(max_length=250, blank=True)
     form_method = models.IntegerField(max_length=10, choices=METHODS, default=METHODS.get)
     form_id = AutoSlugField(populate_from='name', unique=True) 
+    form_class = models.CharField(max_length=250, blank=True)
 
-    # other form attributes
+    # secondary attributes
     form_accept_charset = models.CharField(max_length=100, blank=True)
     form_autocomplete = models.BooleanField(default=False)
     form_novalidate = models.BooleanField(default=False)
@@ -47,10 +52,11 @@ class Form(models.Model):
     form_target = models.CharField(max_length=50, blank=True)
 
 
-    # json field for global and event attributes, including class, id, etc...
-    attrs = jsonfield.JSONField(blank=True)
+    # json field for global and event attributes
+    attrs = hstore.DictionaryField(null=True, blank=True)
 
-    @python_2_unicode_compatible
+    objects = hstore.HStoreManager()
+
     def __str__(self):
         return "formulator.Form instance: %s" % self.name
 
@@ -67,7 +73,7 @@ class Form(models.Model):
         self.full_clean()
 
         attrs = {}
-        fields = OrderedDict() 
+        #fields = OrderedDict()
         layouts = []
 
         for fieldset in self.fieldsets:
@@ -84,23 +90,21 @@ class Form(models.Model):
         helper.form_id = self.form_id
         helper.form_action = self.form_action
         helper.form_method = self.METHODS[self.form_method]
-        helper.attrs = {'accept-charset': self.form_accept_charset,
-                        'autocomplete': self.form_autocomplete,
-                        'novalidate': self.form_novalidate,
-                        'enctype': self.form_enctype,
-                        'target': self.form_target
+        helper.attrs = {
+            'accept-charset': self.form_accept_charset,
+            'autocomplete': self.form_autocomplete,
+            'novalidate': self.form_novalidate,
+            'enctype': self.form_enctype,
+            'target': self.form_target
         }
-
         helper.layout = layout.Layout(*layouts)
 
         attrs['helper'] = helper
-
         return type(str(self.form_id), (form_class,), attrs)
 
 
+@python_2_unicode_compatible
 class FieldSet(models.Model):
-    """
-    """
     form = models.ForeignKey(Form)
     position = PositionField(collection='form')
     name = models.CharField(max_length=100)
@@ -110,23 +114,27 @@ class FieldSet(models.Model):
     def fields(self):
         return self.field_set.all()
 
-    
-    @python_2_unicode_compatible
     def __str__(self):
-        return "Fieldset %s with legend '%s' in form %s" % (self.name, self.legend, self.form.name)
+        return "FieldSet %s with legend '%s' in form %s" % (self.name, self.legend, self.form.name)
 
+
+@python_2_unicode_compatible
 class Field(models.Model):
     """
+    Stores the information for a django form field.
+
     """
+
     formset = models.ForeignKey(FieldSet)
     name = models.CharField(max_length=100, )
     position = PositionField(collection='formset')
 
     field = models.CharField(max_length=100, choices=settings.FORMULATOR_FIELDS)
-    attrs = jsonfield.JSONField()
+    attrs = hstore.DictionaryField(blank=True, null=True)
     slug = AutoSlugField(unique=True, populate_from='name')
 
-    required = models.BooleanField(default=True, help_text=_('Boolean that specifies whether the field is required.'))
+    required = models.BooleanField(default=True,
+                                   help_text=_('Boolean that specifies whether the field is required.'))
     widget = models.CharField(max_length=100, choices=settings.FORMULATOR_WIDGETS, blank=True,
                               help_text=_("""A Widget class, or instance of a Widget class, that should
                                            be used for this Field when displaying it. Each Field has a
@@ -143,11 +151,15 @@ class Field(models.Model):
                                help_text=_("""A value to use in this Field's initial display. This value
                                               is *not* used as a fallback if data isn't given. """))
 
-    help_text = models.TextField(blank=True, help_text=_("An optional string to use as 'help text' for this Field."))
-    show_hidden_initial = models.BooleanField(default=False, help_text=_('Boolean that specifies whether the field is hidden.'))
+    help_text = models.TextField(blank=True,
+                                 help_text=_("An optional string to use as 'help text' for this Field."))
 
-    class Meta:
-        ordering =['position']
+    show_hidden_initial = models.BooleanField(
+        default=False,
+        help_text=_('Boolean that specifies whether the field is hidden.'))
+
+    def __str__(self):
+        return "Field instance: %s" % self.name
 
     def formfield_instance_factory(self, field_class=None, attrs=None):
         """Returns an instance of a form field"""
@@ -176,16 +188,16 @@ class Field(models.Model):
             label = self.label
 
         if attrs is None:
-            attrs = { "required": self.required,
-                      "widget": widget(attrs=self.attrs),
-                      "label": label,
-                      "initial": self.initial, 
-                      "help_text":self.help_text,
-                      "show_hidden_initial": self.show_hidden_initial,
-                    }
+            attrs = {
+                "required": self.required,
+                "widget": widget(attrs=self.attrs),
+                "label": label,
+                "initial": self.initial,
+                "help_text":self.help_text,
+                "show_hidden_initial": self.show_hidden_initial,
+            }
 
         return field(**attrs)
 
-    @python_2_unicode_compatible
-    def __str__(self):
-        return "Form instance: %s" % self.name
+    class Meta:
+        ordering = ['position']
