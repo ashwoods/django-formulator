9ba6bec2ce39e8bab9beb8a24c1fd62a804ca92e
Ashley Camba <ashwoods@gmail.com>
2015-06-25 18:18:57 +0200

    modernizing setup

--- setup.py
+++ setup.py
@@ -1,42 +1,51 @@
-from setuptools import setup, find_packages
-import re
 import os
-import sys
-
-from formulator import get_version
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+import re
+import codecs
+from setuptools import setup
 
 
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
 
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='django-formulator',
-    version=get_version(),
+    version=find_version("formulator", "__init__.py"),
     description='Core library for the formulator service',
     author='Ashley Camba Garrido',
     url='https://github.com/ashwoods/django-formulator',
-    packages=get_packages('formulator'),
-    package_data=get_package_data('formulator'),
-    license='MIT')
+    packages=['formulator'],
+    license='MIT',
+    install_requires=[
+     'django>=1.7',
+     'django-positions',
+     'django-model-utils',
+     'django-appconf',
+     'django-autoslug',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
 
