2aafaf2b2999eedcc4fd104dd5b63cee5a4171bf
cahersan <cahersan@gmail.com>
2016-02-23 10:29:06 +0100

    Created Field Admin with Choices as Inlines

--- formulator/admin.py
+++ formulator/admin.py
@@ -2,18 +2,23 @@ from django.contrib import admin
 from .models import Form, Field, Choice
 
 
-class FieldInline(admin.TabularInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+    fields = ('key', 'value')
+    fk_name = 'field'
+
+class FieldAdmin(admin.ModelAdmin):
     model = Field
-    sortable_field_name = "position"
     extra = 0
     fields = ('name', 'label', 'help_text', 'field_type', 'widget', 'max_length', 'placeholder', 'required', 'position')
+    list_display = ['name', 'label', 'form']
     fk_name = 'form'
-
+    inlines = [ChoiceInline]
 
 class FormAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'form_name', 'form_action', 'form_method', 'form_id', 'form_class']
-    inlines = [FieldInline]
 
 
 admin.site.register(Form, FormAdmin)
-admin.site.register(Choice)
+admin.site.register(Field, FieldAdmin)
