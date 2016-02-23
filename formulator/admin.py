8d1752a7c0b5c28f03e9e99f9c350bc7ea57fd0f
cahersan <cahersan@gmail.com>
2016-02-23 12:48:24 +0100

    Re-included field inlines to form admin

--- formulator/admin.py
+++ formulator/admin.py
@@ -4,8 +4,9 @@ from .models import Form, Field, Choice
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
+    sortable_field_name = "position"
     extra = 0
-    fields = ('key', 'value')
+    fields = ('position', 'key', 'value')
     fk_name = 'field'
 
 class FieldAdmin(admin.ModelAdmin):
@@ -16,8 +17,16 @@ class FieldAdmin(admin.ModelAdmin):
     fk_name = 'form'
     inlines = [ChoiceInline]
 
+class FieldInline(admin.TabularInline):
+    model = Field
+    sortable_field_name = "position"
+    extra = 0
+    fields = ('name', 'label', 'help_text', 'field_type', 'widget', 'max_length', 'placeholder', 'required', 'position')
+    fk_name = 'form'
+
 class FormAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'form_name', 'form_action', 'form_method', 'form_id', 'form_class']
+    inlines = [FieldInline]
 
 
 admin.site.register(Form, FormAdmin)
